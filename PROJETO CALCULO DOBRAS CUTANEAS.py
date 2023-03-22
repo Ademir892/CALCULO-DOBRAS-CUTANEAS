3c2ed2e9ac1e2142fae40c6721dca96d2a5d282d
Ademir Marmitt Traesel <ademirtraesel@hotmail.com>
2023-03-21 22:24:06 -0300

    Criando entrada de dados e validacoes

---
+++ PROJETO CALCULO DOBRAS CUTANEAS.py
@@ -0,0 +1,27 @@
+dobraCutaneaPeitoral = float(input("Informe a dobra cutanea do peitoral: "))
+if dobraCutaneaPeitoral <1 :
+    dobraCutaneaPeitoral = float(input("Informe um valor positivo! "))
+
+dobraCutaneaAbdomen = float(input("Informe a dobra cutanea do abdômen: "))
+if dobraCutaneaAbdomen <1 :
+    dobraCutaneaAbdomen = float(input("Informe um valor positivo! "))
+
+dobraCutaneaTricipital = float(input("Informe a dobra cutanea do tríceps: "))
+if dobraCutaneaTricipital <1 :
+    dobraCutaneaPeitoral = float(input("Informe um valor positivo! "))
+
+dobraCutaneaCoxa = float(input("Informe a dobra cutanea da coxa: "))
+if dobraCutaneaCoxa <1 :
+    dobraCutaneaCoxa = float(input("Informe um valor positivo! "))
+
+dobraCutaneaSupraIliaca = float(input("Informe a dobra cutanea do suprailiaca]: "))
+if dobraCutaneaSupraIliaca <1 :
+    dobraCutaneaSupraIliaca = float(input("Informe um valor positivo! "))
+
+dobraCutaneaAxilarMedia = float(input("Informe a dobra cutanea da axilar média: "))
+if dobraCutaneaAxilarMedia <1 :
+    dobraCutaneaAxilarMedia = float(input("Informe um valor positivo! "))
+
+dobraCutaneaSubescapular = float(input("Informe a dobra cutanea subescapular: "))
+if dobraCutaneaSubescapular <1 :
+    dobraCutaneaSubescapular = float(input("Informe um valor o positivo! "))
